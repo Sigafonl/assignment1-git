3db7393ebe82ae93237a3d06ba3faf6f1f6c4885
sigafonl <fsigafoos@gmail.com>
2021-03-07 12:28:48 -0800

    Initial files for assignment 1

---
+++ Hello.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+import java.util.*;
+import java.io.*;
+
+class Hello{
+    public static void main (String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter Hey.txt or Goodnight.txt: ");
+        String fileName  = keyboard.nextLine();
+        File inFile = new File(fileName);
+        
+        System.out.println();//need an extra space here 
+
+        try{
+            Scanner userInput = new Scanner(inFile);
+
+            while(userInput.hasNextLine()){
+                System.out.println(userInput.nextLine());
+            }
+
+            userInput.close();
+        } 
+        catch (FileNotFoundException e){
+            System.out.println("Hey goober, no file here!");
+            e.printStackTrace();
+        }
+    }
+}
